6059efa3e85d4f957ecae2fde73348e3c6d4de9b
charlie2clarke <charliegclarke2@gmail.com>
2022-10-20 12:17:19 +0100

    test: tested reading actuator msgs

--- src/test/server/test_server.py
+++ src/test/server/test_server.py
@@ -1,27 +1,48 @@
-import sys
-import requests
 from threading import Thread
+
+import requests
 from src.main.server.server import SensorServer
 
 
 def test_server():
     sensor_readings = [{"test_time": {"sensor": 18}}]
+    actuator_readings = [
+        {
+            "name": "LED",
+            "sensor_type": "Temperature",
+            "time": "2022-10-20T10:53:43.442139",
+        }
+    ]
     server = SensorServer(
-        host_name="localhost", port=8082, sensor_readings=sensor_readings
+        host_name="localhost",
+        port=8082,
+        sensor_readings=sensor_readings,
+        actuator_readings=actuator_readings,
     )
 
     thread = Thread(target=server.start)
     thread.daemon = True
     thread.start()
 
-    url = "http://localhost:8082"
+    sensor_url = "http://localhost:8082/sensors"
 
-    response = requests.request("GET", url, headers={}, data={})
+    sensor_res = requests.request("GET", sensor_url, headers={}, data={})
 
-    response_json = response.json()
+    sensor_json = sensor_res.json()
 
-    time_key = list(response_json[0].keys())[0]
+    time_key = list(sensor_json[0].keys())[0]
     assert time_key == "test_time"
-    assert response_json[0][time_key] == {"sensor": 18}
+    assert sensor_json[0][time_key] == {"sensor": 18}
+
+    actuator_url = "http://localhost:8082/actuators"
+
+    actuator_res = requests.request("GET", actuator_url, headers={}, data={})
+    actuator_json = actuator_res.json()
+
+    got = actuator_json[0]
+    print(got)
+    assert got["name"] == "LED"
+    assert got["sensor_type"] == "Temperature"
+    assert got["time"] == "2022-10-20T10:53:43.442139"
 
     server.close()
